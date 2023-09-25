117313b43137e20ce6c8ba76f2a5c16a2dad1b61
anoosm <anoosalathur@gmail.com>
2023-09-25 10:50:32 +0530

    created the splash screen

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,5 +4,5 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
     id ("com.android.library") version "8.0.2" apply false
     id ("com.google.dagger.hilt.android") version "2.45" apply false
-    id ("org.jetbrains.kotlin.plugin.serialization") version "1.8.21"
+    id ("org.jetbrains.kotlin.plugin.serialization") version "1.8.10"
 }
